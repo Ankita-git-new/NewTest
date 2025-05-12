2642a34b7dd1bc56cfbbf32090524ace5bcf1cbc
Ankita-git-new <ankita03jais@gmail.com>
2025-05-12 22:34:36 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template_string
+import random
+
+app = Flask(__name__)
+
+@app.route('/game', methods=['GET', 'POST'])
+def game():
+    number = random.randint(1, 10)
+    message = ""
+    if request.method == "POST":
+        try:
+            guess = int(request.form['guess'])
+            if guess < number:
+                message = "Too low!"
+            elif guess > number:
+                message = "Too high!"
+            else:
+                message = "Correct!"
+        except:
+            message = "Invalid input"
+
+    return render_template_string("""
+        <h1>Guess the Number (1-10)</h1>
+        <form method="post">
+            <input name="guess" type="number" required>
+            <input type="submit" value="Guess">
+        </form>
+        <p>{{message}}</p>
+    """, message=message)
